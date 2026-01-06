aa5ae274d52551431810b0cbe80cc60a7c808c8a
Portugal1576 <andrii.rubtsov@inmost.pro>
2026-01-06 17:19:51 +0000

    Add base screens, navigation, Koin. Clean code

--- app/src/main/java/com/portugal1576/marsrover/presentation/screens/favorites_screen/FavoritesScreenViewModel.kt
+++ app/src/main/java/com/portugal1576/marsrover/presentation/screens/favorites_screen/FavoritesScreenViewModel.kt
@@ -1,7 +1,6 @@
 package com.portugal1576.marsrover.presentation.screens.favorites_screen
 
 import androidx.lifecycle.ViewModel
-import com.portugal1576.marsrover.data.api.MarsRoverRepository
 
 class FavoritesScreenViewModel(): ViewModel() {
 //    TODO
