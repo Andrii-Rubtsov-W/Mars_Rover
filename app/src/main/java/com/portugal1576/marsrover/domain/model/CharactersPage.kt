2a3c9c42f074c9b4d07783d115d31630e9958cdf
Portugal1576 <andrii.rubtsov@inmost.pro>
2026-01-07 18:33:39 +0000

    Cleaned code

--- app/src/main/java/com/portugal1576/marsrover/domain/model/CharactersPage.kt
+++
@@ -1,6 +0,0 @@
-package com.portugal1576.marsrover.domain.model
-
-data class CharactersPage(
-    val items: List<Character>,
-    val canLoadMore: Boolean
-)
--- app/src/main/java/com/portugal1576/marsrover/domain/model/Rick.kt
+++
@@ -1,6 +0,0 @@
-package com.portugal1576.marsrover.domain.model
-
-data class Rick(
-    val info: Info,
-    val results: List<Result>
-)
